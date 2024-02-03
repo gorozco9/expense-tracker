975beeb38feae7f77171fa3126b068aa00399a36
gorozco9 <gorozco9@stu.southtexascollege.edu>
2024-02-03 12:44:55 -0600

    Add transaction component working

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,18 @@
   import Header from './components/Header.vue';
   import Balance from './components/Balance.vue';
   import IncomeExpenses from './components/IncomeExpenses.vue';
+  import AddTransaction from './components/AddTransaction.vue';
   import {ref, computed} from 'vue'
 
-const transactions = ref([
-  {id: 1, text: 'Paycheck', amount: 699.99},
-  {id: 2, text: 'Food', amount: -20},
-  {id: 3, text: 'Bills', amount: -200},
-  {id: 4, text: 'Video Game', amount: -54.11},
-  {id: 5, text: 'Tax Return', amount: 3000},
+const transactions = ref([])
 
-])
+// const transactions = ref([
+//   {id: 1, text: 'Paycheck', amount: 699.99},
+//   {id: 2, text: 'Food', amount: -20},
+//   {id: 3, text: 'Bills', amount: -200},
+//   {id: 4, text: 'Video Game', amount: -54.11},
+//   {id: 5, text: 'Tax Return', amount: 3000},
+// ])
 
 //get the total
 const total = computed(() => {
@@ -19,6 +21,7 @@ const total = computed(() => {
     return acc + transaction.amount
   }, 0)
 })
+
 //get the income by adding all positive values
 const income = computed( () => {
   return transactions.value
@@ -27,25 +30,32 @@ const income = computed( () => {
     return acc + transaction.amount
   }, 0)
 })
+
 //get the expenses by adding all the negative values
 const expense = computed( () => {
   return transactions.value
   .filter((transaction)=> transaction.amount < 0)
   .reduce( (acc, transaction) => {
     return acc + transaction.amount
-
   }, 0)
 })
 
-
+//handle trasnactions submitted
+const handleTransactionSubmitted = (transactionData) => {
+  transactions.value.push({
+    text: transactionData.text,
+    amount: transactionData.amount,
+  })
+}
 
 </script>
-
 
 <template>
   <Header></Header>
   <div class="container">
    <Balance :total="total"></Balance>
    <IncomeExpenses :income="income" :expense="expense"></IncomeExpenses>
+   <AddTransaction @transactionSubmitted="handleTransactionSubmitted"></AddTransaction>
+   <!-- {{ transactions }} -->
   </div>
 </template>
